6242ee7cafc0528d1cf1bf7cd5c52119f6ad1492
Gustavo de Azevedo <111658728+gustavodeazevedo@users.noreply.github.com>
2024-04-28 23:32:19 -0300

    Update index.tsx corrigindo erro

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,11 +1,8 @@
 import { FiPlus } from 'react-icons/fi'
-
 import { currencyFormat } from '../../helpers/currencyFormat'
 import { useCart } from '../../hooks/useCart'
 import { SnackData } from '../../interfaces/SnackData'
-
 import { SkeletonSnack } from './SkeletonSnack'
-
 import { Container } from './styles'
 
 interface SnacksProps {
@@ -17,28 +14,30 @@ export function Snacks({ snacks }: SnacksProps) {
 
   return (
     <Container>
-      {!snacks.length
-        ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
-        : snacks.map((snack) => {
-            const snackExistent = cart.find(
-              (item) => item.snack === snack.snack && item.id === snack.id,
-            )
+      {snacks && snacks.length > 0 ? (
+        snacks.map((snack) => {
+          const snackExistent = cart.find(
+            (item) => item.snack === snack.snack && item.id === snack.id,
+          )
 
-            return (
-              <div key={snack.id} className='snack'>
-                {snackExistent && <span>{snackExistent.quantity}</span>}
-                <h2>{snack.name}</h2>
-                <img src={snack.image} alt={snack.name} />
-                <p>{snack.description}</p>
-                <div>
-                  <strong>{currencyFormat(snack.price)}</strong>
-                  <button type='button' onClick={() => addSnackIntoCart(snack)}>
-                    <FiPlus />
-                  </button>
-                </div>
+          return (
+            <div key={snack.id} className='snack'>
+              {snackExistent && <span>{snackExistent.quantity}</span>}
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button' onClick={() => addSnackIntoCart(snack)}>
+                  <FiPlus />
+                </button>
               </div>
-            )
-          })}
+            </div>
+          )
+        })
+      ) : (
+        [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+      )}
     </Container>
   )
 }
